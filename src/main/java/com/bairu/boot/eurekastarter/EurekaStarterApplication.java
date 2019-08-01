59dc3ed937b46be67a86d861824f05a3cc600502
jerryli <519847900@qq.com>
2019-08-01 17:04:18 +0800

    项目初始化

---
+++ src/main/java/com/bairu/boot/eurekastarter/EurekaStarterApplication.java
@@ -0,0 +1,15 @@
+package com.bairu.boot.eurekastarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bairu/boot/eurekastarter/EurekaStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bairu.boot.eurekastarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaStarterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
